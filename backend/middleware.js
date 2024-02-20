4ad2d50efd96478b4406a198944efd51b44534a0
Prathamesh Madniwale <61145586+prathamesh2525@users.noreply.github.com>
2024-02-20 10:19:59 +0530

    added authMiddleware

---
+++ backend/middleware.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken")
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.statud(403).json({})
+  }
+
+  const token = authHeader.split(" ")[1]
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    req.userId = decoded.userId
+  } catch (error) {
+    return res.status(403).json({})
+  }
+}
+
+module.exports = {
+  authMiddleware,
+}
